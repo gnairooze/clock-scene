6914907438783b86b6c5154ae86c575eeae5fa33
George Nairooze <gnairooze@gmail.com>
2017-10-15 12:43:54 +0200

    fix get day from time object

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function clock() {
     var time = new Date(),
-    hours = time.getHours(),
-    minutes = time.getMinutes(),
-    seconds = time.getSeconds(),
-    year = time.getYear();
+        hours = time.getHours(),
+        minutes = time.getMinutes(),
+        seconds = time.getSeconds(),
+        year = time.getYear();
 
-    if (year < 1000)
-    {
+    if (year < 1000) {
         year += 1900;
     }
 
-    var day = time.getDay(),
-    month = time.getMonth();
-    
-    var dayarray=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-    var montharray=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+    //day of the week
+    var dayOfWeek = time.getDay(),
+        day = time.getDate(),
+        month = time.getMonth();
+
+    var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
     document.querySelectorAll('.clock')[0].innerHTML = leadzero(hours) + ":" + leadzero(minutes) + ":" + leadzero(seconds);
-    document.querySelectorAll('.date')[0].innerHTML = dayarray[day] + ", " + montharray[month] + " " + " " + leadzero(day) + ", " + year;
-  
+    document.querySelectorAll('.date')[0].innerHTML = dayarray[dayOfWeek] + ", " + montharray[month] + " " + " " + leadzero(day) + ", " + year;
+
     function leadzero(num) {
         if (num < 10) {
             num = '0' + num
